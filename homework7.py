5aca21804cd12c0d06f193f9091e8eda63ede973
SirEvgen <sandrofa6666666@gmail.com>
2024-05-24 17:32:37 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,7 @@
+my_dict = {'Antip': 1991, 'Tihon': 1980, 'Epistiniya': 2001, 'Lubava': 2003}
+print(my_dict)
+print(my_dict .get('Tihon'))
+print(my_dict .get('Milorada'))
+my_dict.update({'Evstigney': 1998, 'Avdotya': 2005})
+del my_dict['Epistiniya']
+print(my_dict)
